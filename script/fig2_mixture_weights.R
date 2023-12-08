44489d40116d8132e392964a23362260c15a3675
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-12-08 14:28:27 -0600

    Small fix to fig2_mixture_weights.R.

--- script/fig2_mixture_weights.R
+++ script/fig2_mixture_weights.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 library(cowplot)
 dat <- readRDS("../data/fig_2_data/fitted_weight_data.rds")
-pdat <- data.frame(scale = rep(0:7,times = 32),
-                   k     = rep(1:32,each = 8),
+pdat <- data.frame(scale = rep(0:7,times = 30),
+                   k     = rep(1:30,each = 8),
                    x     = as.vector(dat))
 pdat <- transform(pdat,x = cut(x,c(-1,0.01,0.9,1)))
 p <- ggplot(pdat,aes(x = k,y = scale,fill = x)) +
