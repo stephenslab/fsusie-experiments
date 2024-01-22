fa16e84e1911dcf2060a0be0e3fac28c89be582c
william-denault <willam.denault@gmail.com>
2024-01-22 14:31:09 +0100

    update test fig 2

--- script/brouillon.R
+++ script/brouillon.R
@@ -8,7 +8,7 @@ library(susieR)
 library(wavethresh)
 set.seed(2)
 data(N3finemapping)
-X <- N3finemapping$X[1:120,]
+X <- N3finemapping$X[1:60,]
 
 set.seed(1)
 
@@ -19,7 +19,7 @@ true_pos <- 350
 table (X[,true_pos])
 X[,true_pos] <- (X[,true_pos] - min(X[,true_pos] ))
 table (X[,true_pos])
-for (i in 1:120 ){
+for (i in 1:60 ){
   obs[[i]] <- X[i,true_pos]*effect+ runif(length(effect),max=.5)
 }
 
@@ -53,12 +53,14 @@ plot(pos[idx],effect[50:120][idx])
 
 susie_est <- list()
 h=1
+pips <- list()
 y <- do.call(rbind, obs)
 y <- y[, 50:120]
 for (i in idx){
   
   
   susie_est [[h]]<- susie( X=X,y=y[,i], L=1)$sets
+  pips [[h]]    <-susie( X=X,y=y[,i], L=1)$pip
   h=h+1
 }
 
@@ -66,14 +68,14 @@ susie_est
 
 colnames(X) <- 1:ncol(X)
 
-tt <- susiF(y[,idx] , X, L=1 ,pos=pos[idx] )
-tt$cs
+susif_res <- susiF(y[,idx] , X, L=1 ,pos=pos[idx] )
+susif_res$cs
 plot(pos[idx],effect[50:120][idx])
 
-lines(x=tt$outing_grid , y=tt$fitted_func[[1]])
+lines(x=susif_res$outing_grid , y=susif_res$fitted_func[[1]])
 
-lines(x=tt$outing_grid , y=tt$cred_band[[1]][1,])
-lines(x=tt$outing_grid , y=tt$cred_band[[1]][2,])
+lines(x=susif_res$outing_grid , y=susif_res$cred_band[[1]][1,])
+lines(x=susif_res$outing_grid , y=susif_res$cred_band[[1]][2,])
 
 p <- ggplot()
 to_remove <- which(X[,true_pos]==0.39892578125)
@@ -98,24 +100,271 @@ for ( i in 1:30){
 }
 
  
-myplot <- boxplot(y ~ as.factor(Genotype)*x , data=df  , 
-                  boxwex=0.2 ,
-                  ylab="", 
-                  yaxt="n",
-                  xlab= "",
-                  at= do.call( c,lst_pos),
-                  col=c("tomato","slateblue1" , "green2") ,  
-                  xaxt="n")
-lines(x= .5+1:length(50:120)*(35/(120-50)), y=effect[50:120]+0.25,
-      col="slateblue1", lwd=2)
-lines(x=.5+1:length(50:120)*(35/(120-50)), y=2*effect[50:120]+0.25,
-      col="green2", lwd=2)
-lines(x=.5+1:length(50:120)*(35/(120-50)), y=0*effect[50:120]+0.25,
-      col="tomato", lwd=2)
+SNP_idx <-  unique (do.call ( c, lapply( 1:length(susie_est), function (i){
+  susie_est[[i]]$cs$L1
+}
+                                                        
+  )))
 
+SNP_idx  <- SNP_idx[order(SNP_idx )]
 
+image( abs(cor(X[,SNP_idx])))
+ 
 
 
 library(ggplot2)
 ggplot(df, aes(x=x, y=y, col=as.factor(Genotype)))+
-  geom_point()
+  geom_point(alpha=.2)+
+  geom_smooth(se = FALSE)
+
+
+df1 <- data.frame ( y=effect[50:120]+0.25, x= .5+1:length(50:120)*(35/(120-50)))
+
+df2 <- data.frame ( y=2*effect[50:120]+0.25, x= .5+1:length(50:120)*(35/(120-50)))
+
+
+df3 <- data.frame ( y=0*effect[50:120]+0.25, x= .5+1:length(50:120)*(35/(120-50)))
+
+pos
+which ( effect[50:120]>0)
+ 
+
+is.detected <- !do.call(c,lapply(1:length(susie_est), function (i){
+  is.null(susie_est[[i]]$cs)
+}))
+
+df_label <- data.frame(lab= letters[1:15],
+                       x= unique(df$x)[which(idx >16 & idx <57)], 
+                       y=rep(-0.051, 15),
+                       
+                       col= ifelse(is.detected[which(idx >16 & idx <57)] ==TRUE, "lightblue4",
+                                   "orange4"))
+
+
+ 
+pos_SNP_plot <-seq(0,max ( unique(df$x)),
+                   length.out=length(SNP_idx))
+
+df_SNP_pos <- data.frame( x= pos_SNP_plot,
+                          lab = factor(1:length(SNP_idx)),
+                          y=rep(-0.12, length(SNP_idx))
+                         )
+
+idx_causal_SNP <- which(SNP_idx==350)
+
+
+library(ggplot2)
+library(ggrepel)
+P0 <- ggplot()+
+  geom_point(data=df, aes(x=x, y=y, col=as.factor(Genotype)),alpha=.4)+
+  geom_line (data=df1 , aes ( x=x, y=y ),col="green2",size=2)+
+  geom_line (data=df2 , aes ( x=x, y=y ),col="slateblue1",size=2)+
+  geom_line (data=df3 , aes ( x=x, y=y ),col="tomato",size=2)+
+  geom_label (data=df_label, aes (x=x,y=y, label=lab,fill=col))+
+  geom_point  (data=df_SNP_pos, aes (x=x,y=y ), size=3)+
+   geom_point(  aes( x=df_SNP_pos$x[idx_causal_SNP] ,
+                y=df_SNP_pos$y[idx_causal_SNP] ) , 
+                col="red", size=3) +
+  xlab("")+ylab("")+
+  scale_colour_manual(values= c( "1"= "green2",
+                                 "2"="slateblue1",
+                                 "0"="tomato"))+
+  theme_classic()+
+  theme(legend.position = "none",
+        panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+P0 
+
+tt <- susie_est [[9]] 
+tt0 <- pips[[9]][SNP_idx ]
+
+
+
+df_pip1 <-data.frame(x=pos_SNP_plot,
+                     y=tt0)
+df2   <-data.frame(x= pos_SNP_plot [ which ( SNP_idx %in% tt$cs$L1)] ,
+                   y=tt0[which ( SNP_idx %in% tt$cs$L1)])
+df3   <-data.frame(x= pos_SNP_plot[idx_causal_SNP],
+                   y=tt0[idx_causal_SNP])
+
+P11 <- ggplot( )+
+  geom_point(df_pip1, mapping=aes(x=x, y=y))+
+  xlab("SNP index")+
+  ylab("PIP")+
+  ggtitle("Fine mapping on a")+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  geom_point(df2 ,  mapping=aes(x=x,y=y), col="lightblue3",size=3)+
+  geom_point(df3 ,  mapping=aes(x=x,y=y), col="red", shape=21,size=3)
+
+P11
+
+
+
+
+ 
+
+
+tt <- susie_est [[11]] 
+tt0 <- pips[[11]][SNP_idx ]
+
+
+
+df_pip1 <-data.frame(x=pos_SNP_plot,
+                     y=tt0)
+df2   <-data.frame(x= pos_SNP_plot [ which ( SNP_idx %in% tt$cs$L1)] ,
+                   y=tt0[which ( SNP_idx %in% tt$cs$L1)])
+df3   <-data.frame(x= pos_SNP_plot[idx_causal_SNP],
+                   y=tt0[idx_causal_SNP])
+
+P12 <- ggplot( )+
+  geom_point(df_pip1, mapping=aes(x=x, y=y))+
+  xlab("SNP index")+
+  
+  ggtitle("Fine mapping on c")+
+  ylab("PIP")+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  geom_point(df2 ,  mapping=aes(x=x,y=y), col="lightblue3",size=3)+
+  geom_point(df3 ,  mapping=aes(x=x,y=y), col="red", shape=21,size=3)
+ 
+P12
+
+
+
+
+
+
+
+tt <- susie_est [[13]] 
+tt0 <- pips[[13]][SNP_idx ]
+
+df_pip1 <-data.frame(x=pos_SNP_plot,
+                     y=tt0)
+df2   <-data.frame(x= pos_SNP_plot [ which ( SNP_idx %in% tt$cs$L1)] ,
+                   y=tt0[which ( SNP_idx %in% tt$cs$L1)])
+df3   <-data.frame(x= pos_SNP_plot[idx_causal_SNP],
+                   y=tt0[idx_causal_SNP])
+
+P13 <- ggplot( )+
+  geom_point(df_pip1, mapping=aes(x=x, y=y))+
+  xlab("SNP index")+
+  ylab("PIP")+
+  
+  ggtitle("Fine mapping on d")+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  geom_point(df2 ,  mapping=aes(x=x,y=y), col="lightblue3",size=3)+
+  geom_point(df3 ,  mapping=aes(x=x,y=y), col="red", shape=21,size=3)
+
+P13
+
+
+df_est_f<- data.frame(y=c(  susif_res$fitted_func[[1]],
+                            susif_res$cred_band[[1]][1,],
+                            susif_res$cred_band[[1]][2,]
+                                                    ),
+                      x= rep( susif_res$outing_grid, 3),
+                      type=factor(rep(1:3, each=32)))
+ 
+
+df_effect <- data.frame ( y=  effect[50:120] , 
+                    x=  1:length(50:120)*(35/(120-50)))
+
+
+
+P21 <-ggplot( )+
+  geom_line(df_effect,  mapping=aes(x=x, y=y))+
+  geom_line( df_est_f[which(df_est_f$type==1),],
+             mapping=aes(x=x, y=y,linetype="longdash"), 
+             col="lightblue3",
+             size=1.3)+
+  geom_line( df_est_f[which(df_est_f$type==2),],
+             mapping=aes(x=x, y=y,linetype="solid"),
+             col="lightblue3",
+             size=1.3)+
+  geom_line( df_est_f[which(df_est_f$type==3),],
+             mapping=aes(x=x, y=y,linetype="solid"),
+             col="lightblue3",
+             size=1.3)+
+  
+  xlab("")+
+  ylab("")+
+  theme_classic()+
+  theme(legend.position = "none",
+        panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+P21 
+
+
+
+
+
+tt <- susif_res  
+tt0 <- rep(0,length( SNP_idx  ))
+# susif reindex the ouptut so given that 42 columns are excluded a bit annoying to reindex
+tt0[9]<-1
+#anyhow check in susif_res$cs you will see that full weight in the causal part
+df_pip1 <-data.frame(x=pos_SNP_plot,
+                     y=tt0)
+df2   <-data.frame(x= pos_SNP_plot [9] ,
+                   y=tt0[9])
+df3   <-data.frame(x= pos_SNP_plot[idx_causal_SNP],
+                   y=tt0[idx_causal_SNP])
+P22 <- ggplot( )+
+  geom_point(df_pip1, mapping=aes(x=x, y=y))+
+  xlab("SNP index")+
+  ylab("PIP")+
+  
+  ggtitle("Fine mapping with FSuSiE")+
+  theme_classic()+
+  theme(panel.border = element_rect(colour = "black", fill=NA, size=1.2),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())+
+  geom_point(df2 ,  mapping=aes(x=x,y=y), col="lightblue3",size=3)+
+  geom_point(df3 ,  mapping=aes(x=x,y=y), col="red", shape=21,size=3)
+
+P22
+
+library(gridExtra)
+
+
+grid_plot <- ggdraw()+
+  
+  draw_plot(P0,  x = 0.01 , y = .6, width = .99, height = .39)+
+  draw_plot(P11,  x = 0.01 , y = .4, width = .49, height = .2 )+
+  draw_plot(P12,  x = 0.01 , y = .2, width = .49, height = .2 )+
+  draw_plot(P13,  x = 0.01 , y = .0, width = .49, height = .2 )+
+  
+  draw_plot(P21,  x = 0.5 , y = .3, width = .49, height = .3)+
+  
+  draw_plot(P22,  x = 0.5 , y = .0, width = .49, height = .3)
+ 
+grid_plot
+
+
+ggsave(grid_plot , file="plot/testfig2.pdf",
+       width = 29.7,
+       height = 21,
+       units = "cm"
+)
