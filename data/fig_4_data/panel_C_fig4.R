d27801e4be41cba0a9e3c8dd7338726054a62813
WD <william-denault@users.noreply.github.com>
2025-02-12 13:42:43 +0100

    Update panel_C_fig4.R

--- data/fig_4_data/panel_C_fig4.R
+++ data/fig_4_data/panel_C_fig4.R
@@ -30,17 +30,6 @@ sumstat <- data$d[[5]]
 pip_df <- data$d[[6]]
 
 
-# Parameters
-view_win <- c(4759843, 5000000)
-text_size <- 20
-gene <- c("ENSG00000029725", "ENSG00000161929", "ENSG00000108556")
-custom_labeller <- function(x) {
-  x %>% 
-    gsub("DeJager_", "", ., fixed = TRUE) %>%  
-    gsub("([_:,|-])", "\n", .)             
-}
-# Function to extract SNP position from a given notation
-
 
 plot_df <- data$d[[1]]
 haQTL_df <- data$d[[2]]
@@ -54,20 +43,67 @@ pip_df <- data$d[[6]]
 view_win <- c(4759843, 5000000)
 
 
-
-
+### AD GWAS panel -----
 
 
 
 
 chr =  paste("chr", 17, sep = "")
 
+study="AD_Bellenguez_2022"
+data_track =plot_df[ which(  plot_df$study == study   ),]
+#data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2]& plot_df$region==gene_name  ),]
 
-data_track =plot_df[ which(  plot_df$study == "DLPFC_DeJager_eQTL" ),]
-data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2] ),]
+data_track_CS1 =plot_df [ which(  plot_df$study == study & plot_df$CS1   ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
 
-data_track_CS1 =plot_df [ which(  plot_df$study == "DLPFC_DeJager_eQTL" & plot_df$CS1 ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
-data_track_CS4 =plot_df [ which(  plot_df$study == "DLPFC_DeJager_eQTL" & plot_df$CS4 ),]# plot_df %>% filter(CS4, study == "DLPFC_DeJager_eQTL") #"steelblue"
+
+
+t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track$pos , end = data_track$pos )),
+                data = matrix(data_track$z , nrow=1), genome = "hg19", 
+                type = "p", col = "black",  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+
+t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos , end = data_track_CS1$pos )),
+                data = matrix(data_track_CS1$z , nrow=1), genome = "hg19", 
+                ylim =c( min(data_track$z), max(data_track$z)),
+                type = "p", col = "#2E8B57", cex=1.5,  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+otAD <- OverlayTrack(trackList=list(    t1,  t2 ))
+
+
+plotTracks( otAD )
+
+
+
+
+
+
+#### CHRNE panel ---- 
+
+
+chr =  paste("chr", 17, sep = "")
+
+gene_name="CHRNE"
+study="DLPFC_DeJager_eQTL" 
+data_track =plot_df[ which(  plot_df$study ==study & plot_df$region==gene_name ),]
+#data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2]& plot_df$region==gene_name  ),]
+
+data_track_CS1 =plot_df [ which(  plot_df$study == study & plot_df$CS1& plot_df$region==gene_name  ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
+data_track_CS4 =plot_df [ which(  plot_df$study == study & plot_df$CS4& plot_df$region==gene_name ),]# plot_df %>% filter(CS4, study == "DLPFC_DeJager_eQTL") #"steelblue"
 
 
 
@@ -106,46 +142,125 @@ t3= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_tr
                 col.title = "black") ) # Change title color to black
 
 
-otAD <- OverlayTrack(trackList=list(    t1, t3 ,t2 ))
+otCHRNE <- OverlayTrack(trackList=list(    t1, t3 ,t2 ))
 
 
-plotTracks( otAD)
-
-# second panle
-
-data_ha =pip_df[which( pip_df$study =="ROSMAP_DLPFC_haQTL"&pip_df$cs_coverage_0.95_min_corr==2  ),]
-#  pip_df %>% filter(study %in% c("ROSMAP_DLPFC_haQTL"), cs_coverage_0.95_min_corr == 2)
-data_ha= data_ha[which(data_ha$pos> view_win[1] & data_ha$pos<view_win[2]),]
-t_ha= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_ha$pos , end = data_ha$pos )),
-                  data = matrix(data_ha$pip , nrow=1), genome = "hg19", 
-                  ylim =c( 0, 0.5),
-                  type = "p", col = "steelblue",
-                  cex=1.5,# Use color column from df_plot
-                  track.margin = 0.05, # Reduce margin between track and title
-                  cex.title = 0.6,     # Reduce title size
-                  cex.axis = 0.6,      # Reduce axis text size
-                  col.axis = "black",  # Change axis color to black
-                  col.title = "black") ) # Change title color to black
+plotTracks( otCHRNE )
 
 
-plotTracks( t_ha)
 
 
-data_me =  pip_df[which( pip_df$study %in% c("ROSMAP_DLPFC_mQTL", "") & pip_df$cs_coverage_0.95 == 7),]
-  #pip_df %>% filter(study %in% c("ROSMAP_DLPFC_mQTL", ""), cs_coverage_0.95 == 7)
-data_me= data_me[which(data_me$pos> view_win[1] & data_me$pos<view_win[2]),]
-t_me= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_me$pos , end = data_me$pos )),
-                  data = matrix(data_me$pip , nrow=1), genome = "hg19", 
-                  ylim =c( 0, 0.5),
-                  type = "p", col = "maroon",
-                  cex=1.5,# Use color column from df_plot
-                  track.margin = 0.05, # Reduce margin between track and title
-                  cex.title = 0.6,     # Reduce title size
-                  cex.axis = 0.6,      # Reduce axis text size
-                  col.axis = "black",  # Change axis color to black
-                  col.title = "black") ) # Change title color to black
-otme_ha <- OverlayTrack(trackList=list(    t_me, t_ha  ))
-plotTracks(otme_ha, from =view_win[1], to = view_win[2])
+
+
+
+
+
+
+#### RABEP1 panel ---- 
+
+
+chr =  paste("chr", 17, sep = "")
+
+gene_name="RABEP1"
+study="Mic_DeJager_eQTL" 
+data_track =plot_df[ which(  plot_df$study ==study & plot_df$region==gene_name ),]
+#data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2]& plot_df$region==gene_name  ),]
+
+data_track_CS1 =plot_df [ which(  plot_df$study == study & plot_df$CS1& plot_df$region==gene_name  ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
+data_track_CS4 =plot_df [ which(  plot_df$study == study & plot_df$CS4& plot_df$region==gene_name ),]# plot_df %>% filter(CS4, study == "DLPFC_DeJager_eQTL") #"steelblue"
+
+
+
+t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track$pos , end = data_track$pos )),
+                data = matrix(data_track$z , nrow=1), genome = "hg19", 
+                type = "p", col = "black",  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+
+t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos , end = data_track_CS1$pos )),
+                data = matrix(data_track_CS1$z , nrow=1), genome = "hg19", 
+                ylim =c( min(data_track$z), max(data_track$z)),
+                type = "p", col = "#2E8B57", cex=1.5,  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+otRABEP1 <- OverlayTrack(trackList=list(    t1,  t2 ))
+
+
+plotTracks( otRABEP1 )
+
+
+
+
+
+
+#### SCIMP panel ---- 
+
+
+chr =  paste("chr", 17, sep = "")
+
+gene_name="SCIMP"
+study="Mic_DeJager_eQTL" 
+data_track =plot_df[ which(  plot_df$study ==study & plot_df$region==gene_name ),]
+#data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2]& plot_df$region==gene_name  ),]
+
+data_track_CS1 =plot_df [ which(  plot_df$study == study & plot_df$CS1& plot_df$region==gene_name  ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
+data_track_CS4 =plot_df [ which(  plot_df$study == study & plot_df$CS4& plot_df$region==gene_name ),]# plot_df %>% filter(CS4, study == "DLPFC_DeJager_eQTL") #"steelblue"
+
+
+
+t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track$pos , end = data_track$pos )),
+                data = matrix(data_track$z , nrow=1), genome = "hg19", 
+                type = "p", col = "black",  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+
+t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos , end = data_track_CS1$pos )),
+                data = matrix(data_track_CS1$z , nrow=1), genome = "hg19", 
+                ylim =c( min(data_track$z), max(data_track$z)),
+                type = "p", col = "#2E8B57", cex=1.5,  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black") ) # Change title color to black
+
+
+
+otSCIMP <- OverlayTrack(trackList=list(    t1,  t2 ))
+
+
+plotTracks( otSCIMP )
+
+
+
+list_track=  list( otAD,
+                   otCHRNE,
+                   otRABEP1 ,
+                   otSCIMP
+)
+
+
+plotTracks(list_track,
+           from = min( plot_df$pos[which(plot_df$study=="AD_Bellenguez_2022")]),
+           to=max( plot_df$pos[which(plot_df$study=="AD_Bellenguez_2022")]) )
 
 #### ha ---- ----- 
 
