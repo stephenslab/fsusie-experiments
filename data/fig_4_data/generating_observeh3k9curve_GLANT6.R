3a2f796432cf05d746426a33679a3f10d1131dd3
WD <william.denault@gmail.com>
2025-03-26 11:19:06 +0100

    building CD2AP case study

---
+++ data/fig_4_data/generating_observeh3k9curve_GLANT6.R
@@ -0,0 +1,202 @@
+rm(list=ls(
+  
+))
+library(data.table)
+
+h3k9 <- readRDS("C:/Document/Serieux/Travail/Data_analysis_and_papers/fsusie-experiments/data/fig_4_data/haQTL_GLANT6_raw_peaks.rds")
+
+meta_data= fread("C:/Document/Serieux/Travail/Data_analysis_and_papers/xQTL_data/H3K9ac_count/H3K9acDomains.csv" )
+
+
+Id_map = fread("C:/Document/Serieux/Travail/Data_analysis_and_papers/xQTL_data/H3K9ac_count/sampleSheetAfterQc.csv")
+
+
+
+idcount= as.numeric(unique(h3k9$samples))
+h3k9$IDsample=NA
+
+l_name=rep(NA, length(unique(h3k9$samples)))
+for (i in 1: length(idcount)){
+  
+  
+  
+  if ( length(Id_map$SampleID[which(Id_map$ProjID ==idcount[i])] )>0){
+    h3k9$IDsample[ which( h3k9$samples==unique(h3k9$samples)[i])] =Id_map$SampleID[which(Id_map$ProjID ==idcount[i])]
+  }
+  
+}
+
+
+
+h3k9 = h3k9[-which(is.na(h3k9$IDsample)),]
+h3k9$start <- as.numeric(h3k9$start)
+h3k9$end <- as.numeric(h3k9$end)
+h3k9$normalized_reading <- as.numeric(h3k9$normalized_reading)
+
+
+
+
+
+
+
+
+
+
+data= h3k9
+
+
+h3k9 [which(h3k9$start==h3k9$start[1]),]
+
+
+
+
+plot(data$start[1:20000],
+     data$normalized_reading [1:20000])
+
+
+
+res_ha <- readRDS("C:/Document/Serieux/Travail/Data_analysis_and_papers/fsusie-experiments/data/fig_4_data/fsusie_object/raw_data/ROSMAP_haQTL.chr12_50815042_54677408.fsusie_mixture_normal_top_pc_weights.input_data.rds")
+
+
+ 
+X=as.data.frame(res_ha$residual_X) 
+
+
+lead_SNP= matrix(X[,2716 ], ncol=1)
+
+hist(lead_SNP)
+
+binarized_SNP=0* lead_SNP 
+
+for ( i in 1:nrow(lead_SNP)){
+  if(lead_SNP[i]>0.1){
+    binarized_SNP[i]=1
+    if(lead_SNP[i]>  0.75){
+      binarized_SNP[i]=2
+    }
+  }
+}
+
+table(binarized_SNP)
+
+
+rownames(binarized_SNP)= rownames(X)
+
+h3k9$SNP = rep(0,nrow(h3k9))
+
+h3k9$SNP [which (h3k9$IDsample %in% row.names(binarized_SNP)[which(binarized_SNP==1)])]=1
+
+
+h3k9$SNP [which (h3k9$IDsample %in% row.names(binarized_SNP)[which(binarized_SNP==2)])]=2
+table(h3k9$SNP)
+
+unique(h3k9$samples)
+
+
+h3k9 [c(which(h3k9$samples =="00482428")[1:20],
+        which(h3k9$samples =="00668310")[1:20]),]
+
+
+
+
+
+
+plot(h3k9$normalized_reading [ which(h3k9$samples =="00668310")][1:2000], type="l")
+plot(h3k9$normalized_reading [ which(h3k9$samples =="00482428")][1:2000], type="l")
+
+
+
+
+
+
+
+
+start = min(h3k9$start)
+
+
+end= max(h3k9$end)
+
+obs_pos= start:end
+
+
+mean_func2= 0*obs_pos
+idx = unique( h3k9$IDsample[which (      h3k9$SNP==2)])
+for ( i in 1: length(idx )){
+  
+  sub= h3k9[which(h3k9$IDsample==idx  [i]),]
+  
+  for ( k in 1:nrow(sub)){
+    
+    sub_pos = which( obs_pos>=  sub$start[k ] & obs_pos  <= sub$end[k ])
+    if (length(sub_pos)>0){
+      
+      mean_func2[ sub_pos]= mean_func2[ sub_pos]+sub$normalized_reading[k]
+    }
+    
+  }
+  
+  print(i)
+  
+} 
+
+
+mean_func2=mean_func2/length(idx )
+
+
+
+
+
+
+mean_func1= 0*obs_pos
+idx = unique( h3k9$IDsample[which (      h3k9$SNP==1)])
+for ( i in 1: length(idx )){
+  
+  sub= h3k9[which(h3k9$IDsample==idx  [i]),]
+  
+  for ( k in 1:nrow(sub)){
+    
+    sub_pos = which( obs_pos>=  sub$start[k ] & obs_pos  <= sub$end[k ])
+    if (length(sub_pos)>0){
+      
+      mean_func1[ sub_pos]= mean_func1[ sub_pos]+sub$normalized_reading[k]
+    }
+    
+  }
+  
+  print(i)
+  
+}
+mean_func1=mean_func1/length(idx )
+
+
+
+mean_func0= 0*obs_pos
+idx = unique( h3k9$IDsample[which (      h3k9$SNP==0)])
+for ( i in 1: length(idx )){
+  
+  sub= h3k9[which(h3k9$IDsample==idx  [i]),]
+  
+  for ( k in 1:nrow(sub)){
+    
+    sub_pos = which( obs_pos>=  sub$start[k ] & obs_pos  <= sub$end[k ])
+    if (length(sub_pos)>0){
+      
+      mean_func0[ sub_pos]= mean_func0[ sub_pos]+sub$normalized_reading[k]
+    }
+    
+  }
+  
+  print(i)
+  
+}
+mean_func0=mean_func0/length(idx )
+
+
+
+obs_curve= list(mean_func0=mean_func0,
+                mean_func1=mean_func1,
+                mean_func2=mean_func2,
+                obs_pos=obs_pos)
+
+save(obs_curve,
+     file="C:/Document/Serieux/Travail/Data_analysis_and_papers/fsusie-experiments/data/fig_4_data/row_count_GALNT6.RData")
--- scripts_plot/cases_study/plot_CD2AP_case_study.R
+++ scripts_plot/cases_study/plot_CD2AP_case_study.R
@@ -10,11 +10,21 @@ library(Gviz)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(fsusieR)
 library(dplyr)
+library(data.table)
 path= getwd()
  
   
   path= getwd()
-data = readRDS(paste0(  path,"/data/fig_4_data/Fig4_data.rds") )
+ 
+
+dat <- read.table(paste0(  path,"/data/fig_4_data/CD2AP/CD2AP_plot_df.tsv"),header = TRUE,
+                  sep = "\t",stringsAsFactors = FALSE)
+pdat1 <- subset(dat,
+                study == "AD_Bellenguez_2022" &
+                  pos >= 47.3e6 & pos <= 47.75e6)
+pdat2 <- subset(dat,
+                study == "Exc_DeJager_eQTL" &
+                  pos >= 47.3e6 & pos <= 47.75e6)
 
 extract_snp_position <- function(snp_string) {
   # Split the input string by ':'
@@ -33,14 +43,8 @@ cex=0.6
 
 
 #view_win <- c(56300000, 56700000) 
- view_win <- c(47200000, 47600000)  
-plot_df <- data$f[[1]]
-haQTL_df <- data$f[[2]]
-MSBB_df <- data$f[[3]]
-gene_info <- data$f[[4]]
-sumstat <- data$f[[5]]
-pip_df <- data$f[[6]]
-QTL_data <- data$f[[7]] 
+ view_win <- c(47.3e6, 47.75e6)  
+
 
 
 chr =  paste("chr",6, sep = "")
@@ -48,27 +52,18 @@ chr =  paste("chr",6, sep = "")
 
 
 47472829
-
-
-chr =  paste("chr",12, sep = "")
-
+ 
 
 
 
 ### AD GWAS panel -----
 
-
-study="AD_Bellenguez_2022"
-data_track = plot_df [ which(   plot_df $study == study   ),]
-#data_track = data_track[which(    data_track$pos > view_win[1] &  data_track$pos <view_win[2]& plot_df$region==gene_name  ),]
-
-data_track_CS1 =plot_df [ which(  plot_df$study == study & plot_df$CS1   ),]  #%>% filter(study == "DLPFC_DeJager_eQTL", CS1)#"maroon"
-
-
-
-t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track$pos , end = data_track$pos )),
-                data = matrix(data_track$`-log10(P)` , nrow=1), genome = "hg19",
-                ylim =c( min(data_track$`-log10(P)`), max(data_track$`-log10(P)`)+0.2),
+ 
+pdat1$X.log10.P. 
+t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start =pdat1$pos ,
+                                                                 end = pdat1$pos )),
+                data = matrix(pdat1$X.log10.P. , nrow=1), genome = "hg19",
+                ylim =c( min(pdat1$X.log10.P.), max(pdat1$X.log10.P.)+0.2),
                 type = "p", col = "black",  # Use color column from df_plot
                 track.margin = 0.05, # Reduce margin between track and title
                 cex.title = 0.6,     # Reduce title size
@@ -80,13 +75,14 @@ t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_tr
 
 plotTracks(t1)
 
-
-t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos[1]  , end = data_track_CS1$pos[1]  )),
-                data = matrix(data_track_CS1$`-log10(P)`[1] , nrow=1), genome = "hg19", 
-                ylim =c( min(data_track$`-log10(P)`), max(data_track$`-log10(P)`)+0.2),
+pdat1CS = pdat1[which(pdat1$CS1),]
+t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = pdat1CS$pos   , 
+                                                                 end = pdat1CS$pos   )),
+                data = matrix(pdat1CS$X.log10.P.  , nrow=1), genome = "hg19", 
+                ylim =c( min(pdat1$X.log10.P.), max(pdat1$X.log10.P.)+0.2),
                 type = "p", col = "black", cex=1.5,
                 fill=  "royalblue",
-                pch=c(25 ),# Use color column from df_plot
+                pch=c(24 ),# Use color column from df_plot
                 track.margin = 0.05, # Reduce margin between track and title
                 cex.title = 0.6,     # Reduce title size
                 cex.axis = 0.6,      # Reduce axis text size
@@ -94,24 +90,8 @@ t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_tr
                 col.title = "black",cex.title = cex,
                 rotation.title = 90,
                 background.title = "white",name="AD") ) # Change title color to black
-
-
-t3= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos[-1] , end = data_track_CS1$pos[-1] )),
-                data = matrix(data_track_CS1$`-log10(P)`[-1] , nrow=1), genome = "hg19", 
-                ylim =c( min(data_track$`-log10(P)`), max(data_track$`-log10(P)`)+0.2),
-                type = "p", col = "black", cex=1.5,
-                fill=  "royalblue",
-                pch=c(24),# Use color column from df_plot
-                track.margin = 0.05, # Reduce margin between track and title
-                cex.title = 0.6,     # Reduce title size
-                cex.axis = 0.6,      # Reduce axis text size
-                col.axis = "black",  # Change axis color to black
-                col.title = "black",cex.title = cex,
-                rotation.title = 90,
-                background.title = "white",name="AD") ) 
-
-tidx=which(data_track_CS1$pos==51354542)
-
+ 
+ 
 
 t4= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_track_CS1$pos[tidx] ,
                                                                  end = data_track_CS1$pos[tidx] )),
@@ -128,10 +108,62 @@ t4= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = data_tr
                 rotation.title = 90,
                 background.title = "white",name="AD") ) 
 
-otAD <- OverlayTrack(trackList=list(    t1,  t2,t3 ),
+otAD <- OverlayTrack(trackList=list(    t1,  t2  ),
                      background.title = "white")
 
 
 
-plotTracks( otAD )
+plotTracks( otAD , from= view_win[1],
+            to= view_win[2])
 
+## EQTL panel ----- 
+
+
+
+
+pdat2$X.log10.P. 
+t1= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start =pdat2$pos ,
+                                                                 end = pdat2$pos )),
+                data = matrix(pdat2$X.log10.P. , nrow=1), genome = "hg19",
+                ylim =c( min(pdat2$X.log10.P.), max(pdat2$X.log10.P.)+0.2),
+                type = "p", col = "black",  # Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black",cex.title = cex,
+                rotation.title = 90,
+                background.title = "white",name="AD") ) # Change title color to black
+
+plotTracks(t1)
+
+pdat2CS = pdat2[which(pdat2$CS1),]
+t2= ( DataTrack(range = GRanges(seqnames = chr, ranges = IRanges(start = pdat2CS$pos   , 
+                                                                 end = pdat2CS$pos   )),
+                data = matrix(pdat2CS$X.log10.P.  , nrow=1), genome = "hg19", 
+                ylim =c( min(pdat2$X.log10.P.), max(pdat2$X.log10.P.)+0.2),
+                type = "p", col = "black", cex=1.5,
+                fill=  "royalblue",
+                pch=c(24 ),# Use color column from df_plot
+                track.margin = 0.05, # Reduce margin between track and title
+                cex.title = 0.6,     # Reduce title size
+                cex.axis = 0.6,      # Reduce axis text size
+                col.axis = "black",  # Change axis color to black
+                col.title = "black",cex.title = cex,
+                rotation.title = 90,
+                background.title = "white",name="AD") ) # Change title color to black
+
+
+oteqTL <- OverlayTrack(trackList=list(    t1,  t2  ),
+                     background.title = "white")
+
+plotTracks( oteqTL, from= view_win[1],
+            to= view_win[2])
+
+
+
+# pip plot  -----
+
+res <- readRDS("C:/Document/Serieux/Travail/Data_analysis_and_papers/fsusie-experiments/data/fig_4_data/CD2AP/ROSMAP_mQTL.chr6_44880827_48309905.fsusie_mixture_normal_top_pc_weights.input_data.rds")
+
+res$
