0768d90544635251706b266c262b2f4224885f0a
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-01 09:19:13 -0500

    Made a few improvements to the CR1/CR2 'zoomout' plot.

--- analysis/pip_zoomout_cr1.R
+++ analysis/pip_zoomout_cr1.R
@@ -6,6 +6,7 @@ library(data.table)
 library(ggplot2)
 library(ggrepel)
 library(cowplot)
+set.seed(1)
 zoomin_region <- c(207.4,207.7)
 # Colors are from colorbrewer2.org
 cs_colors <- c("dodgerblue","darkorange","magenta","limegreen","gold")
@@ -17,33 +18,37 @@ pdat <- data.frame(id  = as.character(NA),
                    cs  = as.character(NA),
                    stringsAsFactors = FALSE)
 pdat <- transform(pdat,pos = as.numeric(pos)/1e6)
-rownames(pdat) <- ids
 n <- length(obj_plot$cs_fsusie_obj)
 for (i in 1:n) {
   snps <- names(obj_plot$cs_fsusie_obj[[i]])
-  cs_label <- sprintf("(CS %d, %d SNPs)",i,length(snps))
-  pdat[snps,"cs"] <- i
+  cs_label <- sprintf("CS %d, %d SNPs",i,length(snps))
+  pdat[snps,"cs"] <- cs_label
   j <- snps[which.max(pdat[snps,"pip"])]
-  pdat[j,"id"] <- paste(j,cs_label)
+  pdat[j,"id"] <- sprintf("%s (CS %d)",j,i)
 }
 pdat <- transform(pdat,cs = factor(cs))
+i <- which(pdat$pip >= 0.01)
+j <- which(pdat$pip < 0.01)
+j <- sample(j,2000)
+i <- sort(c(i,j))
+pdat <- pdat[i,]
 p <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
   geom_point(color = "black",size = 0.5) +
   geom_point(data = subset(pdat,!is.na(cs)),mapping = aes(color = cs),
-             shape = 1,size = 1.5) +
-  geom_text_repel(color = "black",size = 2.25,min.segment.length = 0,
-                  max.overlaps = Inf) +
+             shape = 1,size = 1.75) +
+  geom_text_repel(color = "dimgray",size = 2.25,min.segment.length = 0,
+                  max.overlaps = Inf,segment.color = "dimgray") +
   geom_errorbarh(data = data.frame(xmin = zoomin_region[1],
                                    xmax = zoomin_region[2],
                                    y = -0.1),
                  mapping = aes(xmin = xmin,xmax = xmax,y = y),
                  color = "darkgray",linewidth = 0.5,height = 0.05,
                  inherit.aes = FALSE) +
+  scale_x_continuous(breaks = seq(205,209,0.5)) +
   scale_color_manual(values = cs_colors,na.value = "darkgray") +
-  guides(color = "none") +
-# guides(guide_legend(override.aes = list(shape = 20,size = 2))) +
+  guides(color = guide_legend(override.aes = list(shape = 20,size = 2))) +
   labs(x = "base-pair position on chromosome 1 (Mb)",y = "PIP",
        title = "CR1/CR2") +
-  theme_cowplot(font_size = 10)
+  theme_cowplot(font_size = 8)
 print(p)
-ggsave("zoomout_cr1.pdf",p,height = 2,width = 8)
+ggsave("zoomout_cr1.pdf",p,height = 1.75,width = 8)
