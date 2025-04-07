36805c43056f2e6897dbe6af0c370e436bace0d9
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-07 17:17:57 -0500

    Adjusted the 'zoom-in' region in the pip_zoomout_cd2ap.R script.

--- analysis/pip_zoomout_cd2ap.R
+++ analysis/pip_zoomout_cd2ap.R
@@ -11,7 +11,7 @@ library(ggplot2)
 library(ggrepel)
 library(cowplot)
 set.seed(1)
-zoomin_region <- c(47.4,47.7)
+zoomin_region <- c(47.375, 47.725)
 cs_colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99",
                "#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#b15928",
                "#8dd3c7","#bebada","#fb8072","#80b1d3","#fdb462",
