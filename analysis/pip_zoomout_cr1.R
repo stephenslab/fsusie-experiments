49d0afa068bc2af5e58cfc6a2f870a9a488af9fc
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-21 17:37:30 -0500

    I've created a better effect plot for the CR1/CR2 example (pip_zoomout_cr1.R).

--- analysis/pip_zoomout_cr1.R
+++ analysis/pip_zoomout_cr1.R
@@ -13,6 +13,7 @@ library(cowplot)
 set.seed(1)
 zoomin_region <- c(207.45,207.75)
 
+# The top panel is a PIP plot.
 # Colors are from colorbrewer2.org
 cs_colors <- c("dodgerblue","darkorange","magenta","limegreen","gold")
 load("../outputs/CR1_CR2_obj.RData")
@@ -23,13 +24,15 @@ pdat <- data.frame(id  = as.character(NA),
                    cs  = as.character(NA),
                    stringsAsFactors = FALSE)
 pdat <- transform(pdat,pos = as.numeric(pos)/1e6)
+zoomout_region <- range(pdat$pos)
 n <- length(obj_plot$cs_fsusie_obj)
 for (i in 1:n) {
   snps <- names(obj_plot$cs_fsusie_obj[[i]])
   # cs_label <- sprintf("CS %d, %d SNPs",i,length(snps))
   pdat[snps,"cs"] <- i # cs_label
   j <- snps[which.max(pdat[snps,"pip"])]
-  pdat[j,"id"] <- sprintf("%s (CS %d, %d SNPs)",j,i,length(snps))
+  # pdat[j,"id"] <- sprintf("%s (CS %d, %d SNPs)",j,i,length(snps))
+  pdat[j,"id"] <- paste("CS",i)
 }
 pdat <- transform(pdat,cs = factor(cs))
 i <- which(pdat$pip >= 0.01)
@@ -37,12 +40,12 @@ j <- which(pdat$pip < 0.01)
 j <- sample(j,2000)
 i <- sort(c(i,j))
 pdat <- pdat[i,]
-p <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
-  geom_point(color = "black",size = 0.5) +
-  geom_point(data = subset(pdat,!is.na(cs)),mapping = aes(color = cs),
-             shape = 1,size = 1.75) +
-  geom_text_repel(color = "dimgray",size = 2.25,min.segment.length = 0,
+p1 <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
+  geom_point(color = "black",size = 0.65) +
+  geom_point(data = subset(pdat,!is.na(cs)),mapping = aes(color = cs)) +
+  geom_text_repel(color = "black",size = 2.25,min.segment.length = 0,
                   max.overlaps = Inf,segment.color = "dimgray") +
+                  # color = "dimgray"
   geom_errorbarh(data = data.frame(xmin = zoomin_region[1],
                                    xmax = zoomin_region[2],
                                    y = -0.1),
@@ -51,10 +54,56 @@ p <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
                  inherit.aes = FALSE) +
   scale_x_continuous(breaks = seq(205,209,0.5)) +
   scale_color_manual(values = cs_colors,na.value = "darkgray") +
-  # guides(color = guide_legend(override.aes = list(shape = 20,size = 2))) +
   guides(color = "none") +
   labs(x = "base-pair position on chromosome 1 (Mb)",y = "PIP",
        title = "CR1/CR2") +
   theme_cowplot(font_size = 8)
-print(p)
-ggsave("zoomout_cr1.pdf",p,height = 1.75,width = 8)
+
+# The second panel shows the effects on H3K27ac.
+source("../scripts_plot/cases_study/interpolate_effect_estimates.R")
+load("../outputs/CR1_CR2_all_effects.RData")
+n <- length(obj_plot$effect_list)
+effects <- NULL
+for (i in 1:n) {
+  dat <- obj_plot$effect_list[[i]]
+  peak_pos <- dat$peak_pos
+  dat <- data.frame(effect = dat$effect_s$effect_estimate,
+                    up     = dat$effect_s$cred_band["up",],
+                    low    = dat$effect_s$cred_band["low",],
+                    pos    = dat$pos_H3Kac_effect)
+  m   <- length(peak_pos)
+  dat <- interpolate_effect_estimates(dat,peak_pos[seq(2,m-1)])
+  dat <- transform(dat,
+                   pos     = pos/1e6,
+                   cs      = i,
+                   nonzero = !(low <= 0 & up >= 0))
+  effects <- rbind(effects,dat)
+}
+rows    <- sample(nrow(effects))
+effects <- effects[rows,]
+effects <- transform(effects,cs = factor(cs,1:n))
+p2 <- ggplot() +
+  geom_hline(yintercept = 0,linetype = "dotted") +
+  geom_linerange(data = subset(effects,nonzero),
+                 mapping = aes(x = pos,ymin = low,ymax = up,color = cs)) +
+  geom_point(data = effects,
+             mapping = aes(x = pos,y = effect,color = cs,shape = nonzero),
+             size = 1) +
+  geom_errorbarh(data = data.frame(xmin = zoomin_region[1],
+                                   xmax = zoomin_region[2],
+                                   y = -0.5),
+                 mapping = aes(xmin = xmin,xmax = xmax,y = y),
+                 color = "darkgray",linewidth = 0.5,height = 0.03,
+                 inherit.aes = FALSE) +
+  scale_x_continuous(limits = zoomout_region,
+                     breaks = seq(44,49,0.5)) +
+  scale_y_continuous(breaks = seq(-1,1,0.1)) +
+  scale_color_manual(values = cs_colors,na.value = "darkgray",
+                     drop = FALSE) +
+  scale_shape_manual(values = c(4,19)) +
+  guides(color = "none",shape = "none") +
+  labs(x = "base-pair position on chromosome 1 (Mb)",y = "effect") +
+  theme_cowplot(font_size = 8)
+
+print(plot_grid(p1,p2,nrow = 2,ncol = 1,align = "v"))
+# ggsave("zoomout_cr1.pdf",p,height = 1.75,width = 8)
