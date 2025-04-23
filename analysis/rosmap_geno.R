b11e156ba9fe5d247b1200645dc1b7c0f9603ec1
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-22 22:30:32 -0500

    Added a histogram to the rosmap_geno.R analysis script.

--- analysis/rosmap_geno.R
+++ analysis/rosmap_geno.R
@@ -1,5 +1,19 @@
+# First run
+# gzcat ROSMAP_NIA_WGS.leftnorm.bcftools_qc.plink_qc.afreq.gz \
+#   | cut -f 1,6 > temp.tsv
 library(data.table)
-dat <- fread("../data/ROSMAP_NIA_WGS.leftnorm.bcftools_qc.plink_qc.afreq.gz",
-             sep = "\t",stringsAsFactors = FALSE,header = TRUE,nrows = Inf)
+library(ggplot2)
+library(cowplot)
+dat <- fread("../data/temp.tsv",sep = "\t",stringsAsFactors = FALSE,
+             header = TRUE,nrows = Inf)
 class(dat) <- "data.frame"
-hist(log10(dat$ALT_FREQS),n = 64)
+x <- dat$ALT_FREQS
+x <- cut(x,c(0,0.0001,0.001,0.005,0.01,0.05,Inf))
+counts <- table(x)
+pdat <- data.frame(count = as.vector(counts),
+                   bins  = names(counts))
+print(ggplot(pdat,aes(x = bins,y = count)) +
+      geom_col(width = 0.4,color = "white",fill = "royalblue") +
+      labs(x = "MAF",y = "number of SNPs") +
+      theme_cowplot(font_size = 9) +
+      theme(axis.text.x = element_text(angle = 45,hjust = 1)))
