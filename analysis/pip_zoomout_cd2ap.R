6696e8b8b774439c793239c453b6e9754ccd1a83
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-05-01 22:26:28 -0500

    Made a bunch of improvements to pip_zoomout_cd2ap.R.

--- analysis/pip_zoomout_cd2ap.R
+++ analysis/pip_zoomout_cd2ap.R
@@ -13,6 +13,17 @@ library(cowplot)
 set.seed(1)
 zoomin_region <- c(47.375, 47.725)
 
+# Import the Illumina HumanMethylation450 CpG probe information.
+load("../data/IlluminaHumanMethylation450K.rda")
+probes <- IlluminaHumanMethylation450K
+probes <- subset(probes,!is.na(Start_hg38) & !is.na(End_hg38))
+probes <- subset(probes,End_hg38 > Start_hg38)
+probes <- probes[c("CHR_hg38","Start_hg38","End_hg38")]
+names(probes) <- c("chr","start","end")
+probes$cpg <- rownames(probes)
+rownames(probes) <- NULL
+probes <- subset(probes,chr == "chr6")
+
 # The top panel is a PIP plot.
 # Colors are from colorbrewer2.org
 cs_colors <- c("#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99",
@@ -25,29 +36,41 @@ pdat <- data.frame(id  = as.character(NA),
                    pos = sapply(strsplit(ids,":",fixed = TRUE),"[[",2),
                    pip = obj_plot$fsusie_obj_me$pip,
                    cs  = as.character(NA),
+                   cs_idx = as.numeric(NA),
+                   blacklist = FALSE,
                    stringsAsFactors = FALSE)
-pdat <- transform(pdat,pos = as.numeric(pos)/1e6)
-zoomout_region <- range(pdat$pos)
 rownames(pdat) <- ids
 n <- length(obj_plot$fsusie_obj_me$sets$cs)
 for (i in 1:n) {
   snps <- names(obj_plot$fsusie_obj_me$sets$cs[[i]])
-  pdat[snps,"cs"] <- i # cs_label
   j <- snps[which.max(pdat[snps,"pip"])]
-  pdat[j,"id"] <- sprintf("CS %d (%d SNPs, %s)",i,length(snps),j)
+  pdat[snps,"cs_idx"] <- i
+  pdat[snps,"cs"] <- sprintf("CS %d (%d SNPs, %s)",i,length(snps),j)
+  pdat[j,"id"] <- paste("CS",i)
 }
-pdat <- transform(pdat,cs = factor(cs))
+snps <- which(!is.na(pdat$cs))
+for (i in snps) {
+  pos <- pdat[i,"pos"]
+  if (any(probes$start <= pos & pos <= probes$end)) {
+     pdat[i,"blacklist"] <- TRUE
+   }
+}
+blacklisted_cs <- unique(subset(pdat,blacklist)$cs_idx)
+pdat <- subset(pdat,!is.element(cs_idx,blacklisted_cs))
+pdat <- transform(pdat,
+                  cs  = factor(cs),
+                  pos = as.numeric(pos)/1e6)
+zoomout_region <- range(pdat$pos)
 i <- which(pdat$pip >= 0.01)
 j <- which(pdat$pip < 0.01)
 j <- sample(j,2000)
 i <- sort(c(i,j))
 pdat <- pdat[i,]
 p1 <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
-  geom_point(color = "black",size = 0.5) +
-  geom_point(data = subset(pdat,!is.na(cs)),mapping = aes(color = cs),
-             shape = 1,size = 1.75) +
-  geom_text_repel(color = "midnightblue",size = 2.25,min.segment.length = 0,
-                  max.overlaps = Inf,segment.color = "midnightblue") +
+  geom_point(color = "black",size = 0.65) +
+  geom_point(data = subset(pdat,!is.na(cs)),mapping = aes(color = cs)) +
+  geom_text_repel(color = "black",size = 2.25,min.segment.length = 0,
+                  max.overlaps = Inf,segment.color = "black") +
   geom_errorbarh(data = data.frame(xmin = zoomin_region[1],
                                    xmax = zoomin_region[2],
                                    y = -0.1),
@@ -56,70 +79,70 @@ p1 <- ggplot(pdat,aes(x = pos,y = pip,label = id)) +
                  inherit.aes = FALSE) +
   scale_x_continuous(breaks = seq(44,49,0.5)) +
   scale_color_manual(values = cs_colors,na.value = "darkgray") +
-  ylim(-0.2,1.2) +
-  guides(color = "none") +
   labs(x = "base-pair position on chromosome 6 (Mb)",y = "PIP",
        title = "CD2AP") +
-  theme_cowplot(font_size = 8)
-
-stop()
+  theme_cowplot(font_size = 8) +
+  theme(legend.position = "top",
+        legend.text = element_text(size = 6))
 
 # Now create the plot showing the effects on methylation.
 source("../scripts_plot/cases_study/interpolate_effect_estimates.R")
 load("../outputs/CD2AP_all_effects.RData")
 n <- length(obj_plot$effect_list)
-zero_effects <- NULL
-nonzero_effects <- NULL
-for (i in 1:n) {
+good_cs <- setdiff(1:n,blacklisted_cs)
+effects <- NULL
+for (i in good_cs) {
   dat <- obj_plot$effect_list[[i]]
   me_pos <- dat$me_pos
   dat <- data.frame(effect = dat$effect_s$effect_estimate,
                     up     = dat$effect_s$cred_band["up",],
                     low    = dat$effect_s$cred_band["low",],
-                    pos    = dat$pos_est_effect)
+                    pos    = dat$pos_est_effect,
+                    label  = "",
+                    stringsAsFactors = FALSE)
   m   <- length(me_pos)
   dat <- interpolate_effect_estimates(dat,me_pos[seq(2,m-1)])
   dat <- transform(dat,
                    pos = pos/1e6,
                    y   = 0,
-                   cs  = i)
-  zero_effects <- rbind(zero_effects,subset(dat,low <= 0 & up >= 0))
-  nonzero_effects <- rbind(nonzero_effects,subset(dat,!(low <= 0 & up >= 0)))
+                   cs  = paste("CS",i),
+                   nonzero = !(low <= 0 & up >= 0))
+  j <- which.max(abs(dat$effect))
+  dat[j,"label"] <- paste("CS",i)
+  effects <- rbind(effects,dat)
 }
-zero_effects    <- subset(zero_effects,!is.element(pos,nonzero_effects$pos))
-zero_effects    <- transform(zero_effects,cs = factor(cs,1:n))
-nonzero_effects <- transform(nonzero_effects,
-                             cs     = factor(cs,1:n),
-                             effect = sign(effect))
+effects <- transform(effects,cs = factor(cs))
+effects <- transform(effects,effect = effect/abs(up - low))
 p2 <- ggplot() +
   geom_hline(yintercept = 0,linetype = "dotted") +
-  # geom_linerange(data = nonzero_effects,
+  # geom_linerange(data = subset(effects,nonzero),
   #                mapping = aes(x = pos,ymin = low,ymax = up,color = cs)) +
-  geom_point(data = nonzero_effects,
-             mapping = aes(x = pos,y = effect,color = cs),
-             size = 0.75) +
-  geom_point(data = zero_effects,
-             mapping = aes(x = pos,y = y),
-             color = "black",size = 0.75) +
+  geom_point(data = effects,
+             mapping = aes(x = pos,y = effect,color = cs,shape = nonzero)) +
   geom_errorbarh(data = data.frame(xmin = zoomin_region[1],
                                    xmax = zoomin_region[2],
-                                   y = -0.5),
+                                   y = -20),
                  mapping = aes(xmin = xmin,xmax = xmax,y = y),
-                 color = "darkgray",linewidth = 0.5,height = 0.1,
+                 color = "darkgray",linewidth = 0.5,height = 1,
                  inherit.aes = FALSE) +
+  geom_text_repel(data = effects,
+                  mapping = aes(x = pos,y = effect,label = label),
+                  color = "black",size = 2.25,min.segment.length = 0,
+                  max.overlaps = Inf,segment.color = "black") +
   scale_x_continuous(limits = zoomout_region,
                      breaks = seq(44,49,0.5)) +
   scale_color_manual(values = cs_colors,na.value = "darkgray") +
-  guides(color = "none") +
+  scale_shape_manual(values = c(19,19)) +
+  guides(shape = "none") +
   labs(x = "base-pair position on chromosome 6 (Mb)",y = "effect") +
-  theme_cowplot(font_size = 8)
-
-stop()
+  theme_cowplot(font_size = 8) +
+  theme(legend.position = "top",
+        legend.text = element_text(size = 6))
 
 # Create the final combined plot.
-print(plot_grid(p1,p2,nrow = 2,ncol = 1,
-                rel_heights = c(2,1),align = "v"))
-ggsave("zoomout_cd2ap.pdf",
-       plot_grid(p1,p2,nrow = 2,ncol = 1,
-                 rel_heights = c(2,1),align = "v"),
-       height = 3,width = 8)
+print(plot_grid(p1,p2,nrow = 2,ncol = 1,align = "v"))
+
+# ggsave("zoomout_cd2ap.pdf",
+#        plot_grid(p1,p2,nrow = 2,ncol = 1,
+#                  rel_heights = c(2,1),align = "v"),
+#        height = 3,width = 8)
