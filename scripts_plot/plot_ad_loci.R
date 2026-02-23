ed13dd86195cf422d37140fbb2430466e5788216
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-02-23 16:45:21 -0600

    Added steps to compile coloc results in plot_ad_loci.R.

--- scripts_plot/plot_ad_loci.R
+++ scripts_plot/plot_ad_loci.R
@@ -9,7 +9,7 @@ library(cowplot)
 ad_loci <- data.frame(trait = c(rep("haQTL",4),rep("mQTL",14)),
                       region = c("chr1_205117782_208795513",
                                  "chr5_82637805_88412930",
-                                 "chr5_85967320_89904257,",
+                                 "chr5_85967320_89904257",
                                  "chr8_98960936_104073051",
                                  "chr6_44880827_48309905",
                                  "chr10_54746550_59267894",
@@ -26,17 +26,38 @@ ad_loci <- data.frame(trait = c(rep("haQTL",4),rep("mQTL",14)),
                                  "chr21_24561908_27573286",
                                  "chr21_25835755_30175889"))
 
+# Load the relevant coloc results.
+coloc_res <- fread("../outputs/ROSMAP_fsusie_AD_coloc.tsv.gz",
+                   sep = "\t",header = TRUE)
+class(coloc_res) <- "data.frame"
+
 # Load the relevant AD GWAS results.
 ad_gwas <- fread("../outputs/fsusie_AD_overlap_sumstat.tsv.gz",
                  sep = "\t",header = TRUE)
 class(ad_gwas) <- "data.frame"
+
+# Add some columns to ad_loci for storing the coloc results.
+ad_loci <- cbind(ad_loci,
+                 data.frame(study       = "",
+                            PP.H0.abf   = 0,
+                            PP.H1.abf   = 0,
+                            PP.H2.abf   = 0,
+                            L_PP.H3.abf = 0,
+                            L_PP.H4.abf = 0))
 
 # Repeat for each of the selected loci.
 n <- nrow(ad_loci)
 for (i in 1:n) {
   trait <- ad_loci[i,"trait"]
   region <- ad_loci[i,"region"]
-
+  cat("trait =",trait,"|","region =",region,"\n")
+  
+  # Extract the coloc results for the region.
+  cols <- c("PP.H0.abf","PP.H1.abf","PP.H2.abf","L_PP.H3.abf","L_PP.H4.abf")
+  res <- subset(coloc_res,xQTL == sprintf("ROSMAP_%s@%s",trait,region))
+  ad_loci[i,"study"] <- res[1,"AD"]
+  ad_loci[i,cols] <- res[1,cols]
+  
   # Plot the Alzheimer's disease GWAS results.
   dat1 <- subset(ad_gwas,
                  type == ad_loci[i,"trait"] &
@@ -54,5 +75,6 @@ for (i in 1:n) {
     theme_cowplot(font_size = 10)
   print(p1)
   
-  stop()
+  # Save the plots in a PDF.
+  # TO DO.
 }
