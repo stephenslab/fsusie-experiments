27e1bcb8f22a2ac6f2f592d528b257294a0f19b5
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-03-24 12:57:16 -0500

    Removed some loci with AD p-values that are not close to being genome-wide significant.

--- scripts_plot/plot_ad_loci.R
+++ scripts_plot/plot_ad_loci.R
@@ -6,17 +6,17 @@ library(cowplot)
 # This is a script to plot the results from the 14 methylation and
 # histone acetylation loci that overlap with AD loci according to
 # coloc.
-ad_loci <- data.frame(trait = c(rep("haQTL",4),rep("mQTL",14)),
+ad_loci <- data.frame(trait = c(rep("haQTL",2),rep("mQTL",9)),
                       region = c("chr1_205117782_208795513",
-                                 "chr5_82637805_88412930",
-                                 "chr5_85967320_89904257",
+                                 # "chr5_82637805_88412930",
+                                 # "chr5_85967320_89904257",
                                  "chr8_98960936_104073051",
                                  "chr6_44880827_48309905",
-                                 "chr10_54746550_59267894",
-                                 "chr12_109831778_113509918",
-                                 "chr12_111405189_114438276",
-                                 "chr12_111570379_117277693",
-                                 "chr16_21437007_24596316",
+                                 # "chr10_54746550_59267894",
+                                 # "chr12_109831778_113509918",
+                                 # "chr12_111405189_114438276",
+                                 # "chr12_111570379_117277693",
+                                 # "chr16_21437007_24596316",
                                  "chr16_24031743_30613717",
                                  "chr16_27341433_34991551",
                                  "chr17_58565557_62392838",
@@ -24,9 +24,7 @@ ad_loci <- data.frame(trait = c(rep("haQTL",4),rep("mQTL",14)),
                                  "chr18_62476957_68137751",
                                  "chr20_53859688_57519449",
                                  "chr21_24561908_27573286",
-                                 "chr21_25835755_30175889"),
-                      pos0 = c(207,rep(0,17)),
-                      pos1 = c(208,rep(0,17)))
+                                 "chr21_25835755_30175889"))
 
 # Load the allele frequency data.
 load("../data/afreq.RData")
@@ -155,8 +153,9 @@ for (i in 1:n) {
     
   # Save the plots to a PDF.
   # print(plot_grid(p1,p2,nrow = 2,ncol = 1,align = "v"))
-  pdfname <- sprintf("%s_%s_plots.pdf",trait,region)
+  pdfname <- sprintf("plots/%s_%s_plots.pdf",trait,region)
   ggsave(pdfname,
          plot_grid(p1,p2,nrow = 2,ncol = 1,align = "v"),
          height = 3,width = 5)
+  gc(verbose = TRUE)
 }
