97276b8e5ed53492a849ec3157b6871984d7ddc1
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-08-21 23:24:03 -0500

    Added a second analysis to fiberseq_fsusie.R.

--- analysis/fiberseq_fsusie.R
+++ analysis/fiberseq_fsusie.R
@@ -12,7 +12,6 @@ library(susieR)
 library(fsusieR)
 set.seed(1)
 dat <- readRDS("../data/processed_region_data_chr2_212990848_212991848.rds")
-dat <- readRDS("../data/processed_region_data_chr2_212990848_212991848.rds")
 # This is a function we will use in the code chunks below to visualize
 # the results of the fSuSiE fine-mapping analysis.
 pip_plot <- function (fit, causal_snps = NULL, xlab = "") {
@@ -90,5 +89,48 @@ plot(x,colMeans(pheno[i0,]),type = "l",col = "darkblue",
 lines(x,colMeans(pheno[i1,]),type = "l",col = "cyan")
 lines(x,colMeans(pheno[i2,]),type = "l",col = "orangered")
 
-# TO DO NEXT: Try analyzing 
-# processed_region_data_chr1_37690171_37691171.rds.
+# Analysis of processed_region_data_chr1_37690171_37691171.rds.
+set.seed(1)
+dat <- readRDS("../data/processed_region_data_chr1_37690171_37691171.rds")
+
+# Get the SNP genotypes.
+geno <- dat$genotype_dosage_mat
+maf  <- colMeans(geno)/2
+maf  <- pmin(maf,1 - maf)
+j    <- which(maf > 0.05)
+geno <- geno[,j]
+
+# Get the pseudobulked read counts.
+pheno <- NULL
+metA <- as.matrix(dat$metA_mat)
+rows <- which(!apply(is.na(metA),1,any))
+metA <- metA[rows,]
+sample_ids <- dat$rids_df$sample_ID[rows]
+for (sample_id in levels(sample_ids)) {
+  i <- which(sample_ids == sample_id)
+  pheno <- rbind(pheno,colMeans(metA[i,]))
+}
+rownames(pheno) <- levels(sample_ids)
+print(all(rownames(geno) == rownames(pheno)))
+
+fit <- susiF(pheno,geno,L = 2,quantile_trans = FALSE,
+             filter_cs = FALSE,maxit = 100,
+             post_processing = "smash")
+
+pip_plot(fit)
+fit$cs
+
+x <- as.numeric(colnames(pheno))
+out1 <- get_fitted_effect(fit,l = 1,cred_band = TRUE,alpha = 0.05)
+out2 <- get_fitted_effect(fit,l = 2,cred_band = TRUE,alpha = 0.05)
+plot(x,out1$effect,type = "l",col = "dodgerblue",xlab = "base-pair position",
+     ylab = "effect")
+lines(x,out2$effect,col = "orangered")
+
+j <- fit$cs[[1]][1]
+i0 <- which(geno[,j] == 0)
+i1 <- which(geno[,j] == 1)
+plot(x,colMeans(pheno[i0,]),type = "l",col = "darkblue",
+     xlab = "base-pair position")
+lines(x,colMeans(pheno[i1,]),type = "l",col = "orangered")
+
